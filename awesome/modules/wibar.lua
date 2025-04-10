641d2a029178933d7d8097a153d917bc249e13b4
drewgrif <jag@justaguylinux.com>
2025-04-10 16:41:42 -0400

    Update wibar.lua

--- awesome/modules/wibar.lua
+++ awesome/modules/wibar.lua
@@ -4,6 +4,9 @@ local gears = require("gears")
 local beautiful = require("beautiful")
 local widgets = require("widgets")
 
+-- Explicitly require the layouts module to avoid the error
+local layouts = require("modules.layouts")
+
 local M = {}
 
 -- Create a textclock widget
@@ -55,10 +58,20 @@ local function only_focused_clients(c, screen)
     return c == client.focus
 end
 
+-- Filter function to only show occupied or selected tags
+local function filter_occupied_or_selected(t)
+    return #t:clients() > 0 or t.selected
+end
+
 -- Create tags for a screen
-function M.create_tags(s, default_layout)
+function M.create_tags(s)
     -- Create tags 1-12
-    local tags = awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" }, s, default_layout or awful.layout.suit.tile)
+    local tags = awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" }, s, awful.layout.layouts[1])
+    
+    -- Apply specific layouts to tags if layouts module is available
+    if layouts and layouts.set_layouts_per_tag then
+        layouts.set_layouts_per_tag(s)
+    end
     
     return tags
 end
@@ -84,13 +97,33 @@ function M.setup_wibar(s, mylauncher, layouts_module)
                            awful.button({ }, 4, function () awful.layout.inc( 1) end),
                            awful.button({ }, 5, function () awful.layout.inc(-1) end)))
     
-    -- Create a taglist widget
+    -- Create a taglist widget that only shows occupied or selected tags
     s.mytaglist = awful.widget.taglist {
         screen  = s,
-        filter  = awful.widget.taglist.filter.all,
+        filter  = filter_occupied_or_selected,  -- Only show occupied or selected tags
+        style   = {
+            shape = gears.shape.rounded_rect,
+            bg_focus = beautiful.bg_focus,
+            fg_focus = beautiful.fg_focus,
+            bg_occupied = beautiful.bg_minimize,
+            fg_occupied = beautiful.fg_normal,
+        },
         layout = {
-            spacing = 12,
+            spacing = 8,
             layout = wibox.layout.fixed.horizontal
+        },
+        widget_template = {
+            {
+                {
+                    id     = 'text_role',
+                    widget = wibox.widget.textbox,
+                },
+                margins = 6,
+                widget  = wibox.container.margin
+            },
+            id     = 'background_role',
+            shape  = gears.shape.rounded_rect,
+            widget = wibox.container.background,
         },
         buttons = taglist_buttons
     }
@@ -105,18 +138,25 @@ function M.setup_wibar(s, mylauncher, layouts_module)
     -- Create the wibox
     s.mywibox = awful.wibar({ position = "top", screen = s })
 
-    -- Add widgets to the wibox
+    -- Add widgets to the wibox with taglist in the middle
     s.mywibox:setup {
         layout = wibox.layout.align.horizontal,
         { -- Left widgets
             layout = wibox.layout.fixed.horizontal,
             mylauncher,
-            s.mytaglist,
             s.mypromptbox,
         },
-        s.mytasklist, -- Middle widget
+        { -- Middle widget - taglist centered
+            {
+                s.mytaglist,
+                halign = "center",
+                widget = wibox.container.place
+            },
+            layout = wibox.layout.fixed.horizontal
+        },
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
+            s.mytasklist,
             widgets.cpu_widget,
             widgets.mem_widget,
             widgets.fs_widget,
@@ -127,6 +167,27 @@ function M.setup_wibar(s, mylauncher, layouts_module)
             s.mylayoutbox,
         },
     }
+    
+    -- Connect signals to update taglist when clients change
+    client.connect_signal("manage", function(c)
+        s.mytaglist:emit_signal("widget::redraw_needed")
+    end)
+    
+    client.connect_signal("unmanage", function(c)
+        s.mytaglist:emit_signal("widget::redraw_needed")
+    end)
+    
+    client.connect_signal("property::screen", function(c)
+        s.mytaglist:emit_signal("widget::redraw_needed")
+    end)
+    
+    client.connect_signal("tagged", function(c)
+        s.mytaglist:emit_signal("widget::redraw_needed")
+    end)
+    
+    client.connect_signal("untagged", function(c)
+        s.mytaglist:emit_signal("widget::redraw_needed")
+    end)
 end
 
 return M

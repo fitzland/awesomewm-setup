7b0d1b0a0f68d72dfc848bce285a168e1bf1c006
drewgrif <jag@justaguylinux.com>
2025-04-11 18:45:05 -0400

    Update menu.lua

--- awesome/modules/menu.lua
+++ awesome/modules/menu.lua
@@ -5,6 +5,7 @@ local awful = require("awful")
 local beautiful = require("beautiful")
 local hotkeys_popup = require("awful.hotkeys_popup")
 local variables = require("modules.variables")
+local gears = require("gears")
 
 local menu = {}
 
@@ -13,14 +14,22 @@ function menu.init()
     -- Get variables
     local terminal = variables.terminal
     local editor_cmd = variables.editor_cmd
-    local browser = variables.browser
-    local file_manager = variables.file_manager
+    
+    -- Get the correct config path
+    local config_path
+    if awesome.conffile then
+        -- For older versions of AwesomeWM
+        config_path = awesome.conffile
+    else
+        -- For newer versions of AwesomeWM
+        config_path = gears.filesystem.get_configuration_dir() .. "/rc.lua"
+    end
     
     -- Define Awesome menu items
     local myawesomemenu = {
         { "hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
         { "manual", terminal .. " -e man awesome" },
-        { "edit config", editor_cmd .. " " .. awful.conffile },
+        { "edit config", editor_cmd .. " " .. config_path },
         { "restart", awesome.restart },
         { "quit", function() awesome.quit() end },
     }
@@ -28,14 +37,14 @@ function menu.init()
     -- Define application menu items
     local myappmenu = {
         { "terminal", terminal },
-        { "browser", browser },
-        { "file manager", file_manager },
+        { "browser", variables.browser },
+        { "file manager", variables.file_manager },
     }
     
     -- Create the main menu
     menu.main_menu = awful.menu({
         items = {
-            { "awesome", myawesomenu, beautiful.awesome_icon },
+            { "awesome", myawesomemenu, beautiful.awesome_icon },
             { "applications", myappmenu },
             { "open terminal", terminal }
         }

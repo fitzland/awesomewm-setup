b6edde2d90505c0665e923cc5c14c001b9e499d3
drewgrif <jag@justaguylinux.com>
2025-04-11 18:42:00 -0400

    Update menu.lua

--- awesome/modules/menu.lua
+++ awesome/modules/menu.lua
@@ -1,86 +1,55 @@
 -- modules/menu.lua
+-- Menu configuration for AwesomeWM
 
--- Load required libraries
 local awful = require("awful")
 local beautiful = require("beautiful")
 local hotkeys_popup = require("awful.hotkeys_popup")
-
--- Get variables from variables module
 local variables = require("modules.variables")
-local terminal = variables.terminal
-local editor_cmd = variables.editor_cmd
-local browser = variables.browser
-local file_manager = variables.file_manager
 
--- Define awesome menu items
-local myawesomemenu = {
-   { "hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
-   { "manual", terminal .. " -e man awesome" },
-   { "edit config", editor_cmd .. " " .. awesome.conffile },
-   { "restart", awesome.restart },
-   { "quit", function() awesome.quit() end },
-}
+local menu = {}
 
--- Define application menu items
-local myappmenu = {
-    { "browser", browser },
-    { "file manager", file_manager },
-    { "terminal", terminal },
-}
-
--- Check for Debian menu
-local has_debian_menu = false
-if debian_menu then
-    has_debian_menu = true
-end
-
--- Check for freedesktop menu
-local has_freedesktop_menu = false
-if freedesktop_menu then
-    has_freedesktop_menu = true
-end
-
--- Create the main menu
-local mymainmenu
-if has_freedesktop_menu then
-    -- Use freedesktop menu as base if available
-    mymainmenu = freedesktop_menu.build({
-        before = {
-            { "awesome", myawesomemenu, beautiful.awesome_icon },
+-- Initialize the menu
+function menu.init()
+    -- Get variables
+    local terminal = variables.terminal
+    local editor_cmd = variables.editor_cmd
+    local browser = variables.browser
+    local file_manager = variables.file_manager
+    
+    -- Define Awesome menu items
+    local myawesomemenu = {
+        { "hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
+        { "manual", terminal .. " -e man awesome" },
+        { "edit config", editor_cmd .. " " .. awful.conffile },
+        { "restart", awesome.restart },
+        { "quit", function() awesome.quit() end },
+    }
+    
+    -- Define application menu items
+    local myappmenu = {
+        { "terminal", terminal },
+        { "browser", browser },
+        { "file manager", file_manager },
+    }
+    
+    -- Create the main menu
+    menu.main_menu = awful.menu({
+        items = {
+            { "awesome", myawesomenu, beautiful.awesome_icon },
             { "applications", myappmenu },
-        },
-        after = {
             { "open terminal", terminal }
         }
     })
-else
-    -- Otherwise create a simple menu
-    local menu_items = {
-        { "awesome", myawesomemenu, beautiful.awesome_icon },
-        { "applications", myappmenu },
-    }
     
-    -- Add Debian menu if available
-    if has_debian_menu then
-        table.insert(menu_items, { "Debian", debian_menu.Debian_menu.Debian })
-    end
+    -- Create a launcher widget
+    menu.launcher = awful.widget.launcher({
+        image = beautiful.awesome_icon,
+        menu = menu.main_menu
+    })
     
-    -- Add terminal entry
-    table.insert(menu_items, { "open terminal", terminal })
-    
-    mymainmenu = awful.menu({ items = menu_items })
+    -- Make menu accessible globally
+    _G.mymainmenu = menu.main_menu
+    _G.mylauncher = menu.launcher
 end
 
--- Create a launcher widget
-local mylauncher = awful.widget.launcher({ 
-    image = beautiful.awesome_icon,
-    menu = mymainmenu 
-})
-
--- Return the menu elements for use in other modules
-return {
-    awesome_menu = myawesomemenu,
-    app_menu = myappmenu,
-    main_menu = mymainmenu,
-    launcher = mylauncher
-}
+return menu

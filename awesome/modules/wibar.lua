da0f07cad38a9edf9fbbe18c7ace3ab3cf056186
drewgrif <jag@justaguylinux.com>
2025-04-11 19:55:26 -0400

    Update wibar.lua

--- awesome/modules/wibar.lua
+++ awesome/modules/wibar.lua
@@ -1,120 +1,140 @@
--- modules/wibar.lua
-
--- Load required libraries
-local awful = require("awful")
-local wibox = require("wibox")
-local gears = require("gears")
-local beautiful = require("beautiful")
-
--- Get widgets and other modules
-local widgets = require("modules.widgets")
-local variables = require("modules.variables")
-local menu = require("modules.menu")
-
--- Function to create taglist buttons
-local function taglist_buttons()
-    return gears.table.join(
-        awful.button({ }, 1, function(t) t:view_only() end),
-        awful.button({ variables.modkey }, 1, function(t)
-            if client.focus then
-                client.focus:move_to_tag(t)
-            end
-        end),
-        awful.button({ }, 3, awful.tag.viewtoggle),
-        awful.button({ variables.modkey }, 3, function(t)
-            if client.focus then
-                client.focus:toggle_tag(t)
-            end
-        end),
-        awful.button({ }, 4, function() awful.tag.viewnext(awful.screen.focused()) end),
-        awful.button({ }, 5, function() awful.tag.viewprev(awful.screen.focused()) end)
-    )
-end
-
--- Function to create tasklist buttons
-local function tasklist_buttons()
-    return gears.table.join(
-        awful.button({ }, 1, function(c)
-            if c == client.focus then
-                c.minimized = true
-            else
-                c:emit_signal("request::activate", "tasklist", {raise = true})
-            end
-        end),
-        awful.button({ }, 3, function() awful.menu.client_list({ theme = { width = 250 } }) end),
-        awful.button({ }, 4, function() awful.client.focus.byidx(1) end),
-        awful.button({ }, 5, function() awful.client.focus.byidx(-1) end)
-    )
-end
-
 -- Setup for newly connected screens (WITHOUT tag creation)
 local function setup_new_screen(s)
-    -- Create a taglist widget
-    s.mytaglist = awful.widget.taglist {
-    screen = s,
-    filter = awful.widget.taglist.filter.all,
-    buttons = taglist_buttons(),
-    layout = {
-        spacing = 8,
-        layout = wibox.layout.fixed.horizontal
+    -- Create a styled taglist widget
+    local mytaglist = awful.widget.taglist {
+        screen = s,
+        filter = awful.widget.taglist.filter.all,
+        buttons = taglist_buttons(),
+        style = {
+            shape = gears.shape.rounded_rect,
+        },
+        layout = {
+            spacing = 8,
+            layout = wibox.layout.fixed.horizontal
+        },
+        widget_template = {
+            {
+                {
+                    {
+                        id = 'text_role',
+                        widget = wibox.widget.textbox,
+                    },
+                    margins = 6,
+                    widget = wibox.container.margin,
+                },
+                id = 'background_role',
+                widget = wibox.container.background,
+            },
+            shape = gears.shape.rounded_rect,
+            widget = wibox.container.background,
+        },
     }
-}
-    -- Create a tasklist widget
-    s.mytasklist = awful.widget.tasklist {
+    
+    -- Create a tasklist widget with icons and text
+    local mytasklist = awful.widget.tasklist {
         screen = s,
         filter = awful.widget.tasklist.filter.currenttags,
-        buttons = tasklist_buttons()
+        buttons = tasklist_buttons(),
+        style = {
+            shape = gears.shape.rounded_rect,
+        },
+        layout = {
+            spacing = 10,
+            layout = wibox.layout.fixed.horizontal
+        },
+        widget_template = {
+            {
+                {
+                    {
+                        {
+                            id = 'icon_role',
+                            widget = wibox.widget.imagebox,
+                        },
+                        margins = 2,
+                        widget = wibox.container.margin,
+                    },
+                    {
+                        id = 'text_role',
+                        widget = wibox.widget.textbox,
+                    },
+                    layout = wibox.layout.fixed.horizontal,
+                },
+                margins = 4,
+                widget = wibox.container.margin
+            },
+            id = 'background_role',
+            widget = wibox.container.background,
+        },
+    }
+    
+    -- Separator widget
+    local separator = wibox.widget {
+        {
+            forced_width = 2,
+            shape = gears.shape.rounded_bar,
+            widget = wibox.widget.separator,
+            color = beautiful.fg_normal .. "33", -- semi-transparent
+        },
+        left = 10,
+        right = 10,
+        widget = wibox.container.margin
     }
     
     -- Create the wibox with explicit settings
     s.mywibox = awful.wibar({ 
         position = "top", 
         screen = s,
-        height = 24,
-        bg = beautiful.bg_normal
+        height = 32, -- slightly taller
+        bg = beautiful.bg_normal,
+        shape = function(cr, width, height)
+            gears.shape.partially_rounded_rect(cr, width, height, false, false, true, true, 8)
+        end
     })
     
+    -- Create a background with gradient
+    local bg_widget = wibox.container.background()
+    bg_widget.bg = {
+        type = "linear",
+        from = { 0, 0 },
+        to = { s.geometry.width, 0 },
+        stops = { 
+            { 0, beautiful.bg_normal .. "ee" },  -- slightly transparent
+            { 0.5, "#112233ee" }, 
+            { 1, beautiful.bg_normal .. "ee" }
+        }
+    }
+
     -- Add widgets to the wibox
     s.mywibox:setup {
-        layout = wibox.layout.align.horizontal,
-        { -- Left widgets
-            layout = wibox.layout.fixed.horizontal,
-            menu.launcher,
-            s.mytaglist,
+        bg_widget,
+        {
+            layout = wibox.layout.align.horizontal,
+            { -- Left widgets
+                layout = wibox.layout.fixed.horizontal,
+                menu.launcher,
+                mytaglist,
+                separator,
+                s.mypromptbox or wibox.widget.textbox(""),
+            },
+            mytasklist, -- Middle widget
+            { -- Right widgets
+                layout = wibox.layout.fixed.horizontal,
+                widgets.cpu_widget,
+                separator,
+                widgets.mem_widget,
+                separator,
+                widgets.date_widget,
+                widgets.time_widget,
+                separator,
+                wibox.widget.systray(),
+            },
         },
-        s.mytasklist, -- Middle widget
-        { -- Right widgets
-            layout = wibox.layout.fixed.horizontal,
-            widgets.cpu_widget,
-            widgets.mem_widget,
-            widgets.date_widget,
-            widgets.time_widget,
-            widgets.systray,
-        },
+        widget = wibox.container.background
     }
+    
+    -- Store the widgets for later access
+    s.mytaglist = mytaglist
+    s.mytasklist = mytasklist
     
     print("Wibar set up on screen " .. s.index)
 end
-
--- Initialize function
-local function init()
-    -- Make sure menu is initialized
-    if not menu.launcher then
-        menu.init()
-    end
-
-    -- Setup existing screens directly
-    for s in screen do
-        setup_new_screen(s)
-    end
-    
-    -- Also setup signal handler for any future screens
-    screen.connect_signal("request::desktop_decoration", function(s)
-        setup_new_screen(s)
-    end)
-end
-
--- Return the module
-return {
-    init = init
-}
